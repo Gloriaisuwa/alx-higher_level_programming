b245a73f0bd50e20119869f83333a8b659db0a90
Gloriaisuwa <gloriaisuwa@gmail.com>
2023-11-30 23:31:27 -0800

    ByteCode -> Python #3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,7 +3,9 @@ if __name__ == "__main__":
     import sys
 
     total = 0
-    for i in range (len(sys.argv) - 1):
+    count = len(sys.argv) -1
+
+    for i in range(count):
         total += int(sys.argv[i + 1])
-        print("{}".format(total))
+        print("{:d}".format(total))
 
