24773a9184b348e38734ce2e10045633778a694a
Gloriaisuwa <gloriaisuwa@gmail.com>
2023-12-06 13:57:54 -0800

    Roman to Integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not roman_string or type(roman_string) != str:
+    if not type(roman_string, str) or roman_string is None:
         return 0
     result = 0
     values = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
