638afc2813b7ffd5f5f41adf069790a56877bdfe
Gloriaisuwa <gloriaisuwa@gmail.com>
2023-12-19 15:16:44 -0800

    Square with size

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python3
 
-
-"""This module defines a class Square that represents a square"""
+"""A class Square that defines a square based on 0-square.py"""
 
 
 class Square:
+
+    """
+    Attributes:
+        size: Private instance attribute representing the size of the square
+    """
+
     def __init__(self, size):
+
+        """
+        Args:
+            size: Size of the square.
+        """
+
         self.__size = size
