19914e48238297f652c006da2c3984675d0b6e26
Gloriaisuwa <gloriaisuwa@gmail.com>
2024-03-28 11:33:21 -0700

    update

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,19 +1,14 @@
 #!/usr/bin/python3
 """
-This module contain a  script that takes in a URL and an email
+This script takes in a URL and an email, sends a POST request to the passed
+URL with the email as a parameter, and displays the body of the response
+(decoded in utf-8).
 """
-import sys
 import urllib.request
-import urllib.parse
-url = sys.argv[1]
-email = sys.argv[2]
+import sys
 
-data = {'email': email}
-data = urllib.parse.urlencode(data)
-data = data.encode('ascii')
-
-request = urllib.request.Request(url, data)
-
-with urllib.request.urlopen(request) as response:
-    content = response.read().decode('utf-8')
-    print(content)
+if __name__ == "__main__":
+    data = urllib.parse.urlencode({'email': sys.argv[2]}).encode('utf-8')
+    req = urllib.request.Request(sys.argv[1], data)
+    with urllib.request.urlopen(req) as response:
+        print(response.read().decode('utf-8'))
